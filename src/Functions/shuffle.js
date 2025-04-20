6d89831c9f3f6d3ca417168c756002ef09e7f56f
adityaaa07 <97532274+adityaaa07@users.noreply.github.com>
2025-04-21 01:12:05 +0530

    Update shuffle.js

--- src/Functions/shuffle.js
+++ src/Functions/shuffle.js
@@ -1,9 +1,27 @@
 import { db } from "../firebase-config";
 import { updateDoc, doc } from "firebase/firestore";
 
-const shuffule = async(image,title,id,channelName,songs,name) =>{
-    const randomIndex = Math.floor(Math.random()*songs.length+1)
-    songs.splice(randomIndex,0,{image,title,id,channelName,playedBy:name})
-    await updateDoc(doc(db,'room',sessionStorage.getItem('roomCode')),{currentSong:[...songs]}).catch(err=>console.log(err)) 
-}
-export default shuffule
+const shuffule = async (image, title, id, channelName, songs, name, platform, uri = null) => {
+  const newTrack = {
+    image,
+    title,
+    id,
+    channelName,
+    platform,
+    playedBy: name,
+  };
+
+  if (platform === 'spotify' && uri) {
+    newTrack.uri = uri;
+  }
+
+  const randomIndex = Math.floor(Math.random() * (songs.length + 1));
+  songs.splice(randomIndex, 0, newTrack);
+
+  await updateDoc(
+    doc(db, 'room', sessionStorage.getItem('roomCode')),
+    { currentSong: [...songs] }
+  ).catch(err => console.log(err));
+};
+
+export default shuffule;
