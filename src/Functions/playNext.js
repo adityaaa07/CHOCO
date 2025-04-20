ab6163c314519d4339e9d21519891afe54701211
adityaaa07 <97532274+adityaaa07@users.noreply.github.com>
2025-04-21 01:05:49 +0530

    Update playNext.js

--- src/Functions/playNext.js
+++ src/Functions/playNext.js
@@ -1,10 +1,29 @@
 import { db } from "../firebase-config";
 import { updateDoc, doc } from "firebase/firestore";
-const playNext=async(image,title,id,channelName,songs,currentPlaying,name)=>{
-    if(songs){
-        const index = songs.findIndex(data => data.id === currentPlaying.id)
-        songs.splice(index+1,0,{image,title,id,channelName,playedBy:name})
-        await updateDoc(doc(db,'room',sessionStorage.getItem('roomCode')),{currentSong:[...songs]}).catch(err=>console.log(err)) 
-    } 
-}
+
+const playNext = async (image, title, id, channelName, songs, currentPlaying, name, platform, uri = null) => {
+  if (songs) {
+    const newTrack = {
+      image,
+      title,
+      id,
+      channelName,
+      platform,
+      playedBy: name,
+    };
+
+    if (platform === 'spotify' && uri) {
+      newTrack.uri = uri;
+    }
+
+    const index = songs.findIndex(data => data.id === currentPlaying.id);
+    songs.splice(index + 1, 0, newTrack);
+    
+    await updateDoc(
+      doc(db, 'room', sessionStorage.getItem('roomCode')),
+      { currentSong: [...songs] }
+    ).catch(err => console.log(err));
+  }
+};
+
 export default playNext;
