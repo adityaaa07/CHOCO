ce0b623f496b72e8941623cfcc22a193c85faaad
Anusha Bhat <130855041+anushhabhat@users.noreply.github.com>
2025-04-20 23:25:30 +0530

    Update SpotifyAuth.jsx

--- src/Components/SpotifyAuth.jsx
+++ src/Components/SpotifyAuth.jsx
@@ -12,7 +12,7 @@ const SpotifyAuth = () => {
       'streaming',
     ];
 
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}`;
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}`;
 
     window.location.href = authUrl;
   };
