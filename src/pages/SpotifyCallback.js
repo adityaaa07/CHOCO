f2280b0befb9b8ddef76bcb629c4962266551e0f
adityaaa07 <97532274+adityaaa07@users.noreply.github.com>
2025-04-28 20:07:24 +0530

    Update SpotifyCallback.js

--- src/pages/SpotifyCallback.js
+++ src/pages/SpotifyCallback.js
@@ -1,61 +1,5 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-const SpotifyCallback = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const getToken = async () => {
-      const code = new URLSearchParams(window.location.search).get("code");
-
-      if (!code) {
-        console.error("No code found in callback URL");
-        return;
-      }
-
-      try {
-        const params = new URLSearchParams();
-        params.append("grant_type", "authorization_code");
-        params.append("code", code);
-        params.append("redirect_uri", process.env.REACT_APP_SPOTIFY_REDIRECT_URI);
-        params.append("client_id", process.env.REACT_APP_SPOTIFY_CLIENT_ID);
-        params.append("client_secret", process.env.REACT_APP_SPOTIFY_CLIENT_SECRET);
-
-        const response = await axios.post("https://accounts.spotify.com/api/token", params, {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        });
-
-        const { access_token, refresh_token, expires_in } = response.data;
-
-        // Save tokens in localStorage or Firebase (temporary: localStorage)
-        localStorage.setItem("spotifyAccessToken", access_token);
-        localStorage.setItem("spotifyRefreshToken", refresh_token);
-        localStorage.setItem("spotifyExpiresIn", Date.now() + expires_in * 1000);
-
-        console.log("Spotify tokens saved!");
-
-        navigate("/"); // Go back to homepage
-      } catch (error) {
-        console.error("Error fetching Spotify token:", error);
-      }
-    };
-
-    getToken();
-  }, [navigate]);
-
-  return (
-   <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl">Connecting to Spotify...</h1>
-    </div>
-  );
-};
-
-export default SpotifyCallback;
-
-/*import axios from 'axios';
+import axios from 'axios';
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useStateContext } from "../Context/ContextProvider";
@@ -89,4 +33,4 @@ const SpotifyCallback = () => {
 };
 
 export default SpotifyCallback;
-*/
+
