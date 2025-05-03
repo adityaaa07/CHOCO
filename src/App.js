25d83f4437862f4c0026a2e147d60e713f429d0e
adityaaa07 <97532274+adityaaa07@users.noreply.github.com>
2025-05-03 23:47:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+/* latest
 import React,{useEffect, useState} from 'react';
 import SpotifyCallback from './pages/SpotifyCallback';
 import './App.css';
@@ -60,6 +62,68 @@ function App() {
       </StyledText>
     </div>
     </>
+  );
+}
+
+export default App;
+*/
+import React, { useEffect } from 'react';
+import SpotifyCallback from './pages/SpotifyCallback';
+import './App.css';
+import './index.css';
+import Index from './pages';
+import { Route, Routes } from 'react-router-dom';
+import Search from './pages/Search';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
+import Homepage from './pages/Homepage';
+import styled from 'styled-components';
+import { useStateContext } from './Context/ContextProvider';
+import Profile from './pages/Profile';
+import Privacypolicy from './pages/Privacypolicy';
+import Terms from './pages/Terms';
+import Thirdparty from './pages/Thirdparty';
+
+const StyledText = styled.div`
+  font-family: "Poppins", sans-serif;
+`;
+
+function App() {
+  document.body.style.backgroundColor = '#0000';
+  const { pathName, setPathName } = useStateContext();
+
+  useEffect(() => {
+    const getPathName = () => {
+      if (window.location.pathname === '/') {
+        setPathName('login');
+      } else {
+        setPathName(window.location.pathname);
+      }
+    };
+    getPathName();
+  }, [window.location.pathname, setPathName]);
+
+  return (
+    <div className="bg-black h-screen">
+      <StyledText>
+        {pathName !== 'login' && (
+          <div className="bg-black mx-auto">
+            <Homepage />
+          </div>
+        )}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Index />} />
+          <Route path="/callback" element={<SpotifyCallback />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/privacy-policy" element={<Privacypolicy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/third-party" element={<Thirdparty />} />
+        </Routes>
+      </StyledText>
+    </div>
   );
 }
 
