b43b571756a9a04bfbc4d2e6bef3d4a65beb4176
Anusha Bhat <130855041+anushhabhat@users.noreply.github.com>
2025-05-03 15:59:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,79 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ContextProvider } from './Context/ContextProvider';
 import SpotifyCallback from './pages/SpotifyCallback';
-import './App.css';
-import './index.css';
 import Index from './pages';
-import { Route, Routes,Router } from 'react-router-dom';
 import Search from './pages/Search';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 import Homepage from './pages/Homepage';
-import styled from 'styled-components'; // or import { css } from '@emotion/react';
-import { useStateContext } from './Context/ContextProvider';
 import Profile from './pages/Profile';
-import Cookies from 'js-cookie';
-import SpotifyPlayer from './Components/SpotifyPlayer';
 import Privacypolicy from './pages/Privacypolicy';
 import Terms from './pages/Terms';
 import Thirdparty from './pages/Thirdparty';
-// Define a styled component using the imported font
+import JoinRoom from './Components/JoinRoom';
+import QueuePanel from './Components/QueuePanel';
+import styled from 'styled-components';
+import { useStateContext } from './Context/ContextProvider';
+import './App.css';
+import './index.css';
+
+// Define styled component for font
 const StyledText = styled.div`
-font-family: "Poppins", 'sans-serif'
+  font-family: "Poppins", sans-serif;
 `;
+
 function App() {
-  // Ensure Spotify SDK is always loaded
-  const showSpotifyPlayer = true;
-  document.body.style.backgroundColor='#0000'
-  const {pathName,setPathName}=useStateContext()
-  useEffect(()=>{
-    const getPathName=()=>{
-      if(window.location.pathname==='/'){
-        setPathName('login')
-      }else{
-        setPathName(window.location.pathname)
+  const { pathName, setPathName } = useStateContext();
+
+  useEffect(() => {
+    const getPathName = () => {
+      const currentPath = window.location.pathname;
+      if (currentPath === '/') {
+        setPathName('login');
+      } else {
+        setPathName(currentPath);
       }
-    }
-   getPathName()
-  },[window.location.pathname])
- 
+    };
+    getPathName();
+  }, [setPathName]);
+
   return (
-    <>
-      {showSpotifyPlayer && <SpotifyPlayer />}
-    <div className='bg-black h-screen'> 
-      <StyledText>
-      {
-        pathName!=='login' &&(<div className='bg-black mx-auto '>
-        <Homepage /> 
-        </div>)
-      }
-      <Routes>
-      <Route path='/' element={<Login  />} />
-        <Route path='/home' element={<Index />} />
-        <Route path="/callback" element={<SpotifyCallback />} />
-        <Route path='/search' element={<Search  />} />
-        <Route path='/chat' element={<Chat  />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/privacy-policy' element={<Privacypolicy />} />
-        <Route path='/terms' element={<Terms />} />
-        <Route path='/third-party' element={<Thirdparty />} />
-      </Routes>
-      </StyledText>
-    </div>
-    </>
+    <ContextProvider>
+      <Router>
+        <StyledText>
+          <div className="min-h-screen bg-black text-white">
+            {/* Player Container for Spotify and YouTube players */}
+            <div id="player-container" className="w-full h-[200px] mb-4"></div>
+
+            {/* Conditional Homepage rendering */}
+            {pathName !== 'login' && (
+              <div className="mx-auto">
+                <Homepage />
+              </div>
+            )}
+
+            {/* Routes */}
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Index />} />
+              <Route path="/callback" element={<SpotifyCallback />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/privacy-policy" element={<Privacypolicy />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/third-party" element={<Thirdparty />} />
+              <Route path="/join-room" element={<JoinRoom />} />
+            </Routes>
+
+            {/* Persistent Components */}
+            <QueuePanel />
+            <JoinRoom />
+          </div>
+        </StyledText>
+      </Router>
+    </ContextProvider>
   );
 }
 
