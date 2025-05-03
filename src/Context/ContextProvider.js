3227c97d75b4f6a8c5bd6b1940705dfaeed0328a
Anusha Bhat <130855041+anushhabhat@users.noreply.github.com>
2025-05-03 15:04:36 +0530

    Update ContextProvider.js

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -1,46 +1,92 @@
-import React , {useContext,createContext,useState,useEffect,useRef} from "react";
-import {collection,getDoc,query,where,orderBy,onSnapshot,doc,getDocs, addDoc, Timestamp, updateDoc,} from 'firebase/firestore'
-import Cookies from 'js-cookie'
-import { db } from '../firebase-config'
-const StateContext=createContext();
-export const ContextProvider=({children})=>{
-    const [videoId,setVideoId]=useState('')
-    const [modal_backdrop,setmodal_backdrop]=useState(false)
-    const [modal_backdrop1,setmodal_backdrop1]=useState(false)
-    const [joineeSong,setJoineeSong]=useState('')
-    const [pathName,setPathName]=useState('')
-    const [videoIds,setVideoIds]=useState([])
-    const [notification,setNotification]=useState(0)
-    const [messages,setMessages]=useState([])
-    const [isLeaving,setIsLeaving] = useState(false)
-    const [onReady,setOnReady] = useState()
-    const [title,setTitle] = useState('')
-    const [artist,setArtist]=useState('')
-    const [currentPlaying,setCurrentPlaying] = useState('')
-    const [duration, setDuration] = useState(0);
-    const [currentTime, setCurrentTime] = useState(0);
-    const [isSeeking, setIsSeeking] = useState(false);
-    const [playedBy,setPlayedBy] = useState('')
-    const seekBarRef = useRef(null);
-    const [token, setToken] = useState(null); // Make sure you have token and setToken
-    useEffect(()=>{
-        const getData=()=>{
-            if(sessionStorage.getItem('roomCode')){
-                const filteredUsersQuery = query(collection(db,'room',sessionStorage.getItem('roomCode'),'messages'),orderBy('timestamp','asc'));
-                onSnapshot(filteredUsersQuery,((data) => {
-                  setMessages(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-                  const unReadMsg =  data.docs.filter((doc)=>(doc.data().status==='unread' && doc.data().sender!==Cookies.get('name')))
-                  setNotification(unReadMsg.length)
-                }
-                ))
-            }
-        }
-        getData()
-        
-    },[sessionStorage.getItem('roomCode')])
-    return (<StateContext.Provider value={{videoId,setVideoId,modal_backdrop,setmodal_backdrop,modal_backdrop1,setmodal_backdrop1,joineeSong,setJoineeSong,pathName,setPathName,notification,setNotification,videoIds,setVideoIds,messages,setMessages,isLeaving,setIsLeaving,setOnReady,onReady,title,setTitle,artist,setArtist,currentPlaying,setCurrentPlaying,duration,setDuration,currentTime,setCurrentTime,isSeeking,setIsSeeking,seekBarRef,playedBy,setPlayedBy,setToken}}>
-        {children}
-    </StateContext.Provider>)
-}
-export const useStateContext = () => useContext(StateContext);
+import React, { useContext, createContext, useState, useEffect, useRef } from "react";
+import { collection, query, orderBy, onSnapshot, doc, addDoc, Timestamp, updateDoc } from 'firebase/firestore';
+import Cookies from 'js-cookie';
+import { db } from '../firebase-config';
 
+const StateContext = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const [videoId, setVideoId] = useState('');
+  const [modal_backdrop, setmodal_backdrop] = useState(false);
+  const [modal_backdrop1, setmodal_backdrop1] = useState(false);
+  const [joineeSong, setJoineeSong] = useState('');
+  const [pathName, setPathName] = useState('');
+  const [videoIds, setVideoIds] = useState([]);
+  const [notification, setNotification] = useState(0);
+  const [messages, setMessages] = useState([]);
+  const [isLeaving, setIsLeaving] = useState(false);
+  const [onReady, setOnReady] = useState(null);
+  const [title, setTitle] = useState('');
+  const [artist, setArtist] = useState('');
+  const [currentPlaying, setCurrentPlaying] = useState(null);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const [playedBy, setPlayedBy] = useState('');
+  const [token, setToken] = useState(sessionStorage.getItem('spotify_token') || null);
+  const seekBarRef = useRef(null);
+
+  useEffect(() => {
+    const getData = () => {
+      const roomCode = sessionStorage.getItem('roomCode');
+      if (roomCode) {
+        const messagesQuery = query(
+          collection(db, 'room', roomCode, 'messages'),
+          orderBy('timestamp', 'asc')
+        );
+        onSnapshot(messagesQuery, (data) => {
+          const messageData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+          setMessages(messageData);
+          const unReadMsg = messageData.filter(
+            (doc) => doc.status === 'unread' && doc.sender !== Cookies.get('name')
+          );
+          setNotification(unReadMsg.length);
+        });
+
+        // Subscribe to room data
+        const roomRef = doc(db, 'room', roomCode);
+        onSnapshot(roomRef, (docSnap) => {
+          if (docSnap.exists()) {
+            const data = docSnap.data();
+            setVideoIds(Array.isArray(data.currentSong) ? data.currentSong : []);
+            if (data.currentPlaying) {
+              setCurrentPlaying(data.currentPlaying);
+              setTitle(data.currentPlaying.title);
+              setArtist(data.currentPlaying.channelName);
+              setPlayedBy(data.currentPlaying.playedBy);
+            }
+          }
+        });
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <StateContext.Provider value={{
+      videoId, setVideoId,
+      modal_backdrop, setmodal_backdrop,
+      modal_backdrop1, setmodal_backdrop1,
+      joineeSong, setJoineeSong,
+      pathName, setPathName,
+      notification, setNotification,
+      videoIds, setVideoIds,
+      messages, setMessages,
+      isLeaving, setIsLeaving,
+      onReady, setOnReady,
+      title, setTitle,
+      artist, setArtist,
+      currentPlaying, setCurrentPlaying,
+      duration, setDuration,
+      currentTime, setCurrentTime,
+      isSeeking, setIsSeeking,
+      seekBarRef,
+      playedBy, setPlayedBy,
+      token, setToken,
+    }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
