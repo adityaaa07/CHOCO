2b9a55fce93e5c22432ac78292028099cbf24632
Anusha Bhat <130855041+anushhabhat@users.noreply.github.com>
2025-05-04 13:23:14 +0530

    Update ContextProvider.js

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { auth } from './firebase-config';
+import { auth } from '../firebase-config';
 import { onAuthStateChanged } from 'firebase/auth';
 
 const StateContext = createContext();
